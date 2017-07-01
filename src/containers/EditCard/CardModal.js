505756bb8d7f142829745611bcabbe0195329d45
Alex Anthony <alex.anthony28991@gmail.com>
2017-07-01 22:19:33 +0100

    Styling for edit dialog

--- src/containers/EditCard/CardModal.js
+++ src/containers/EditCard/CardModal.js
@@ -4,8 +4,20 @@ import { connect } from 'react-redux'
 
 import { EditActions } from '../../reducers/edit'
 
+const modalStyle = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)'
+  },
+  content: {
+    left: '30%',
+    right: '30%',
+    bottom: '30%',
+    backgroundColor: '#edeff0'
+  }
+}
+
 const EditModal = ({ showModal, onHideModal }) =>
-  <Modal isOpen={showModal} onRequestClose={onHideModal} />
+  <Modal isOpen={showModal} onRequestClose={onHideModal} style={modalStyle} />
 
 EditModal.propTypes = {
   showModal: PropTypes.bool,
