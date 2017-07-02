534811bf0dbee2183f201540cd300ad3f449d7f4
Alex Anthony <alex.anthony28991@gmail.com>
2017-07-02 23:34:15 +0100

    Make whole new list placeholder clickable

--- src/containers/Board/NewListPlaceholder.js
+++ src/containers/Board/NewListPlaceholder.js
@@ -2,11 +2,9 @@ import React, { PropTypes } from 'react'
 
 const NewListPlaceholder = ({ onAddList }) =>
   <div className="desk-container new-list-placeholder-container">
-    <div className="desk new-list-placeholder">
+    <div className="desk new-list-placeholder" onClick={onAddList}>
       <div className="desk-head new-list-head">
-        <a href="#" onClick={onAddList}>
-          Add a list...
-        </a>
+        <a href="#">Add a list...</a>
       </div>
     </div>
   </div>
