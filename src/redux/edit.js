1313dd2c985c242fc6ee075d65ffda1a3875c330
Alex Anthony <alex.anthony28991@gmail.com>
2017-07-19 20:49:46 +0100

    Automatically open newly-created card

--- src/redux/edit.js
+++ src/redux/edit.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { REHYDRATE } from 'redux-persist/constants'
 
+import { actionTypes as ListActionTypes } from './lists'
+
 const actionTypes = {
   SET_EDIT_CARD: 'SET_EDIT_CARD',
   DISMISS_EDIT_CARD: 'DISMISS_EDIT_CARD'
@@ -17,6 +19,7 @@ const editCard = (state = null, action) => {
   let incoming
   switch (action.type) {
     case actionTypes.SET_EDIT_CARD:
+    case ListActionTypes.ADD_CARD:
       return action.cardId
     case actionTypes.DISMISS_EDIT_CARD:
       return null
